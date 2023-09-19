62fd927a5494282318a8f504976a03289a04df4d
Yatin1923 <yatinchokshidsa@gmail.com>
2023-09-20 00:38:43 +0530

    Added whole page and cursor dependencies

---
+++ script.js
@@ -0,0 +1,13 @@
+// var cursor = new MouseFollower();
+
+// // Skew effect
+// const skew = document.querySelector('#main');
+// skew.addEventListener('mouseenter', () => {
+//     cursor.setSkewing(3);
+// });
+
+// skew.addEventListener('mouseleave', () => {
+//     cursor.removeSkewing();
+// });
+
+
